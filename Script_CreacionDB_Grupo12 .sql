96fcddbefe4a11bca40c33b488ca739f25cfd8a3
MorenoMat <104392363+MorenoMat@users.noreply.github.com>
2026-06-27 21:03:50 -0300

    Add unique constraints and triggers to database tables Moreno

--- Script_CreacionDB_Grupo12 .sql
+++ Script_CreacionDB_Grupo12 .sql
@@ -6,7 +6,7 @@ go
 
 -- ============================================================
 -- TABLA: Clientes
-
+-- ============================================================
 create table dbo.Clientes
 (
 idCliente int identity(1,1) primary key,
@@ -14,7 +14,7 @@ DNI varchar(20) not null unique,
 Apellido varchar(50) not null,
 Nombre varchar(50) not null,
 Telefono varchar(20) not null,
-Email varchar(100) not null,
+Email varchar(100) not null unique,
 Direccion varchar(150) not null,
 FechaAlta date not null default getdate()
 )
@@ -37,7 +37,7 @@ ApellidoMecanico varchar(50) not null
 create table dbo.Repuestos
 (
 idRepuesto int identity(1,1) primary key,
-nombreRepuesto varchar(50) not null,
+nombreRepuesto varchar(50) not null unique,
 descripcionRepuesto varchar(60) not null,
 precio decimal(10,2) not null default 0 check(precio >= 0),
 stock int not null check(stock >= 0)
@@ -55,7 +55,7 @@ Patente varchar(10) not null unique,
 Marca varchar(50) not null,
 Modelo varchar(50) not null,
 Anio int not null check(anio >= 1990),
-Color varchar(30) not null,
+Color varchar(30)  null,
 FechaAlta date not null default getdate(),
 foreign key (idCliente) references dbo.Clientes (idCliente)
 )
@@ -69,9 +69,9 @@ idPresupuesto int identity(1,1) primary key,
 idVehiculo int not null,
 idMecanico int not null,
 fechaPresupuesto date not null default getdate(),
-fechaEstimadaFin date null,
+fechaEstimadaFin date not null,
 descripcion varchar(400) not null,
-importeTotal decimal(10,2),
+importeTotal decimal(10,2)null,
 estado varchar(20) not null check (estado in ('Pendiente','Aprobado','Rechazado','Finalizado'))
 default 'Pendiente',
 foreign key (idVehiculo) references dbo.Vehiculos (idVehiculo),
@@ -108,7 +108,7 @@ idPresupuesto int not null,
 descripcionTrabajo varchar(300) not null,
 idRepuesto int not null,
 cantidad int not null check(cantidad > 0),
-precio decimal(10,2) not null,
+precio decimal(10,2) not null check(precio> 0),
 foreign key (idRepuesto) references dbo.Repuestos (idRepuesto),
 foreign key (idPresupuesto) references dbo.Presupuestos (idPresupuesto),
 constraint UQ_DetallePresupuesto
@@ -144,3 +144,31 @@ foreign key(idReparacion, idMecanico)
 constraint CK_TareasReparacion_Fechas
     check (fechaFin is null or fechaInicio is null or fechaFin >= fechaInicio)
 )
+go
+-- luego de cambiar  estado de reparaciones a completada, 
+--te pone en fechaFin getdate()
+-- recuerden q inserted es para q solo modifique lo q el usuario acaba de update
+Create Trigger TR_ReparacionCompletaGuardaFechaFin 
+ON dbo.Reparaciones
+AFTER UPDATE 
+AS BEGIN
+IF UPDATE(estado)
+BEGIN
+UPDATE R
+SET R.fechaFin = GETDATE()
+FROM dbo.Reparaciones R
+INNER JOIN inserted i On R.idReparacion = i.idReparacion 
+WHERE i.estado = 'Completada';
+end
+end;
+go
+CREATE TRIGGER TR_DetallePresupuesto_AcumularImporte
+ON dbo.DetallePresupuesto
+AFTER INSERT
+AS
+BEGIN
+    UPDATE P
+    SET P.importeTotal = ISNULL(P.importeTotal, 0) + (i.cantidad * i.precio)
+    FROM dbo.Presupuestos P
+    INNER JOIN inserted i ON P.idPresupuesto = i.idPresupuesto;
+END;
